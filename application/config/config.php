a9e4771c2c02bfeebf384b3332a0302edf100367
ridho pratama <ridhopratamavd@gmail.com>
2017-06-22 15:16:34 +0700

    maneh

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/absensi/';
-$config['base_url'] = 'http://192.168.1.13/absensi/';
+//$config['base_url'] = 'http://192.168.1.13/absensi/';
 
 /*
 |--------------------------------------------------------------------------

f6b89d531c6fc0fae556cd1006ca5d1b41b5bc33
naayunir <naayunir@gmail.com>
2017-07-14 11:18:20 +0700

    update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,13 +1,16 @@
 <div class="container">
 	<div class="col-md-12">
 		<div class="row">
+			<br/>
 			<div class="col-md-6">
-				<br/><h1>Selamat Datang, -nama user-</h1>
+				<h1>Selamat Datang, -nama user-</h1>
 				<h3>Juli 2015</h3>		
 			</div>
 			<div class="col-md-6">
-				<button class="btn btn-primary btn-lg">TAMBAH ABSENSI</button>
-				<button class="btn btn-primary btn-lg">TAMBAH IZIN</button>
+				<div class="dist">
+					<button class="btn btn-primary btn-lg">TAMBAH ABSENSI</button>
+					<button class="btn btn-primary btn-lg">TAMBAH IZIN</button>
+				</div>
 			</div>
 		</div>
 	</div>

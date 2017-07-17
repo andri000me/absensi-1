73c3995386198d905f51e7c63819c2f3c904b77f
ridho pratama <ridhopratamavd@gmail.com>
2017-07-17 11:18:19 +0700

    revisi bulanan

--- application/views/Overview/bulanan.php
+++ application/views/Overview/bulanan.php
@@ -1,59 +1,3 @@
-	<!-- <div class="col-xs-12 col-sm-8 col-sm-push-1" >
-
-		
-	
-	<h4>Laporan Absensi Bulanan</h4><br>
-		<form action="<?php echo base_url('overview_c/lihat/bulan')?>" method="POST" class="form-horizontal" role="form" >
-			<div class="form-group">
-				<label for="inputEmail3" class="col-sm-3 control-label">Tahun</label>
-			    <div class="col-sm-5">
-                	<select class="form-control" data-placeholder="Nama Karyawan"  name="l_tahun">
-                		<?php $date = date('Y'); 
-                			for($x = $date; $x>=2017; $x--) { ?>
-			        	<option value="<?php echo $x;?>"><?php echo $x; ?></option>
-			        	<?php } ?>
-				    </select>
-			    </div>
-			</div>
-			<div class="form-group">
-				<label  class="col-sm-3 control-label">Bulan</label>
-			    <div class="col-sm-5">
-                	<select class="form-control" data-placeholder="Nama Karyawan"  name="l_bulan">
-			        	<option value="01">Januari </option>
-			        	<option value="02">Februari </option>
-			        	<option value="03">Maret </option>
-			        	<option value="04">April </option>
-			        	<option value="05">Mei </option>
-			        	<option value="06">Juni </option>
-			        	<option value="07">Juli </option>
-			        	<option value="08">Agustus </option>
-			        	<option value="09">September </option>
-			        	<option value="10">Oktober </option>
-			        	<option value="11">November </option>
-			        	<option value="12">Desember </option>
-				    </select>
-			    </div>
-			</div>
-			<div class="form-group">
-				<div class="col-sm-10 col-sm-offset-1">
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</div>
-			</div>
-		</form>
-	</div> -->
-
-	<!-- start content -->
-<<<<<<< HEAD:application/views/bulanan.php
-	<div class="container">
-		<div class="col-sm-12">
-			<div class="row">
-			    <div class="col-sm-3">
-			    	<br/><h2>Stasistik Bulanan</h2>
-			    	<h4>Juli 2015</h4>	
-			    </div>
-			    <div class="col-sm-2"></div>
-			    
-=======
 	<div class="col-sm-12">
 		<div class="container">
 		    <div class="col-sm-3">
@@ -62,7 +6,6 @@
 		    </div>
 		    <div class="col-sm-3"></div>
 			<form action="<?php echo base_url('Overview_C/lihat/bulan')?>" method="POST" class="form-horizontal" role="form" >
->>>>>>> 34963018e0322e1db4a9e623b519415a6b7d0c0d:application/views/Overview/bulanan.php
 			    <div class="col-sm-2 distance">
 		            <select class="form-control" data-placeholder="Nama Karyawan"  name="l_tahun">
 		            	<?php $date = date('Y'); 
@@ -73,11 +16,8 @@
 					</select>
 				</div>
 				<div class="col-sm-2 distance">
-<<<<<<< HEAD:application/views/bulanan.php
 		            <select class="form-control" data-placeholder="Nama Karyawan"  name="l_bulan">
-=======
 		            <select class="form-control" data-placeholder="Nama Karyawan"  name="l_bulan" >
->>>>>>> 34963018e0322e1db4a9e623b519415a6b7d0c0d:application/views/Overview/bulanan.php
 		                <option value="00">Pilih Bulan </option>
 					    <option value="01">Januari </option>
 					    <option value="02">Februari </option>
@@ -94,19 +34,6 @@
 					</select>
 				</div>
 			    <div class="col-sm-1 distance">
-<<<<<<< HEAD:application/views/bulanan.php
-			    	<botton  class="btn btn-primary btn-laporan">TAMPILKAN</botton>
-			    </div> 
-			    <div class="col-sm-1 distance">
-			    	<hr class="vertical-line">
-			    </div>
-			 
-			    <div class="col-sm-1 distance">
-			    	
-			    	<botton  class="btn btn-primary btn-laporan">CETAK</botton >
-			    </div>	
-			</div>        
-=======
 			    	<button type="submit" class="btn btn-primary">TAMPILKAN</button>
 			    </div> 
 			</form>
@@ -114,7 +41,6 @@
 		    	<hr class="vertical-line">
 		    	<button  class="btn btn-primary btn-laporan distance2" id="print_btn" >CETAK</button>
 		    </div>	    
->>>>>>> 34963018e0322e1db4a9e623b519415a6b7d0c0d:application/views/Overview/bulanan.php
 	    </div>
     </div>
     <hr class="horizontal-line">
